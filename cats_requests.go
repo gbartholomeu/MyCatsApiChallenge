7c88e30de6bb64ea7923ed3d01fe2c0a3b5d96e8
Gabriel Pinheiro Bartholomeu <gabriel_bartholomeu@hotmail.com>
2020-07-11 18:39:47 -0300

    Requesting cats with hats and glasses

--- cats_requests.go
+++ cats_requests.go
@@ -13,27 +13,32 @@ import (
 
 var (
 	catsDatabase = CreateDBLink()
-	apiKey       = "<my_auth_key>"
+	apiKey       = "<my_api_key>"
 )
 
 type CatsData struct {
-	Id               string     `json:"id"`
-	Name             string     `json:"name"`
-	Temperament      string     `json:"temperament"`
-	Origin           string     `json:"origin"`
-	Description      string     `json:"description"`
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Temperament string `json:"temperament"`
+	Origin      string `json:"origin"`
+	Description string `json:"description"`
 }
 
 type CatsImages struct {
-	Id     string `json:"id"`
-	Url    string `json:"url"`
+	Id  string `json:"id"`
+	Url string `json:"url"`
+}
+
+type ImagesCategories struct {
+	Id   int8   `json:"id"`
+	Name string `json:"name"`
 }
 
 func main() {
 	FillDBWithCatsInfo()
 	FillDBWithCatsImages()
-	// FillDBWithCatsImagesWithHats()
-	// FillDBWithCatsImagesWithGlasses()
+	FillDBWithCatsImagesWithHats()
+	FillDBWithCatsImagesWithGlasses()
 }
 
 // ===== REQUESTS CODES =====
@@ -66,7 +71,7 @@ func FillDBWithCatsInfo() {
 }
 
 func FillDBWithCatsImages() {
-	fmt.Println("Fetching images")
+	fmt.Println("Fetching cats images")
 	myCats := FetchCatsIDName()
 	var myCatImages []CatsImages
 	for _, cat := range myCats {
@@ -97,71 +102,94 @@ func FillDBWithCatsImages() {
 }
 
 func FillDBWithCatsImagesWithHats() {
-	fmt.Println("Fetching images")
-	myCats := FetchCatsIDName()
+	fmt.Println("Fetching images of cats with hats")
 	var myCatImages []CatsImages
-	for _, cat := range myCats {
-		client := &http.Client{}
-		targetBreedRequest := fmt.Sprintf("https://api.thecatapi.com/v1/images/search?breed_id=%s&limit=3&mime_types=jpg,png", cat.Id)
-		req, err := http.NewRequest("GET", targetBreedRequest, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-		req.Header.Set("api_key", apiKey)
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-		bodyText, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		errJSON := json.Unmarshal(bodyText, &myCatImages)
-		if errJSON != nil {
-			log.Println(errJSON)
-		}
-		fmt.Println(fmt.Sprintf("Filling DB with %s images", cat.Name))
-		for _, catImageInfo := range myCatImages {
-			InsertCatImage(catImageInfo, cat.Id, cat.Name)
-		}
+	client := &http.Client{}
+	targetBreedRequest := fmt.Sprintf("https://api.thecatapi.com/v1/images/search?limit=3&mime_types=jpg,png&category_ids=%d", GetCatCategories("hats"))
+	req, err := http.NewRequest("GET", targetBreedRequest, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
+	req.Header.Set("api_key", apiKey)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errJSON := json.Unmarshal(bodyText, &myCatImages)
+	if errJSON != nil {
+		log.Println(errJSON)
+	}
+	fmt.Println("Filling DB with images of cats with hats")
+	for _, catImageInfo := range myCatImages {
+		InsertStylishCatImage(catImageInfo, false, true)
+	}
+
 }
 
 func FillDBWithCatsImagesWithGlasses() {
-	fmt.Println("Fetching images")
-	myCats := FetchCatsIDName()
+	fmt.Println("Fetching images of cats with glasses")
 	var myCatImages []CatsImages
-	for _, cat := range myCats {
-		client := &http.Client{}
-		targetBreedRequest := fmt.Sprintf("https://api.thecatapi.com/v1/images/search?breed_id=%s&limit=3&mime_types=jpg,png", cat.Id)
-		req, err := http.NewRequest("GET", targetBreedRequest, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-		req.Header.Set("api_key", apiKey)
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-		bodyText, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		errJSON := json.Unmarshal(bodyText, &myCatImages)
-		if errJSON != nil {
-			log.Println(errJSON)
-		}
-		fmt.Println(fmt.Sprintf("Filling DB with %s images", cat.Name))
-		for _, catImageInfo := range myCatImages {
-			InsertCatImage(catImageInfo, cat.Id, cat.Name)
+	client := &http.Client{}
+	targetBreedRequest := fmt.Sprintf("https://api.thecatapi.com/v1/images/search?limit=3&mime_types=jpg,png&category_ids=%d", GetCatCategories("sunglasses"))
+	req, err := http.NewRequest("GET", targetBreedRequest, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("api_key", apiKey)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errJSON := json.Unmarshal(bodyText, &myCatImages)
+	if errJSON != nil {
+		log.Println(errJSON)
+	}
+	fmt.Println("Filling DB with images of cats with glasses")
+	for _, catImageInfo := range myCatImages {
+		InsertStylishCatImage(catImageInfo, true, false)
+	}
+}
+
+func GetCatCategories(targetCategory string) int8 {
+	var imagesCategories []ImagesCategories
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.thecatapi.com/v1/categories", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("api_key", apiKey)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errJSON := json.Unmarshal(bodyText, &imagesCategories)
+	if errJSON != nil {
+		log.Println(errJSON)
+	}
+	for _, categoriesInfo := range imagesCategories {
+		if categoriesInfo.Name == targetCategory {
+			return categoriesInfo.Id
 		}
 	}
+	return -1
 }
 
 // ===== DB CODES =====
 
 func CreateDBLink() *sql.DB {
-	db, err := sql.Open("mysql", "root:<My_db_pwd>@tcp(localhost:6603)/cats_api")
+	db, err := sql.Open("mysql", "root:<my_db_pwd>@tcp(localhost:6603)/cats_api")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -190,6 +218,18 @@ func InsertCatImage(cat_image CatsImages, breed_id string, breed_name string) {
 	}
 	defer stmt.Close() // Prepared statements take up server resources and should be closed after use.
 	_, err = stmt.Exec(cat_image.Id, breed_id, breed_name, cat_image.Url)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func InsertStylishCatImage(cat_image CatsImages, has_glasses bool, has_hat bool) {
+	stmt, err := catsDatabase.Prepare("INSERT INTO stylish_cats_images(id, image_url, has_glasses, has_hat) VALUES (?,?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close() // Prepared statements take up server resources and should be closed after use.
+	_, err = stmt.Exec(cat_image.Id, cat_image.Url, has_glasses, has_hat)
 	if err != nil {
 		log.Fatal(err)
 	}
--- createDBStructure.sql
+++ createDBStructure.sql
@@ -3,3 +3,4 @@ USE cats_api;
 CREATE TABLE cats_breeds (id VARCHAR(255) NOT NULL, breed_name VARCHAR(255) NOT NULL, temperament VARCHAR(255), origin VARCHAR(255), 
 breed_description VARCHAR(500),  PRIMARY KEY (id));
 CREATE TABLE cats_images (id VARCHAR(255) NOT NULL, breed_id VARCHAR(255) NOT NULL, breed_name VARCHAR(255) NOT NULL,  image_url VARCHAR(255) NOT NULL, PRIMARY KEY (id), INDEX breed_ind (breed_id), FOREIGN KEY (breed_id) REFERENCES cats_breeds(id) ON DELETE CASCADE);
+CREATE TABLE stylish_cats_images (id VARCHAR(255) NOT NULL, image_url VARCHAR(255) NOT NULL, has_glasses BOOLEAN, has_hat BOOLEAN, PRIMARY KEY (id));
